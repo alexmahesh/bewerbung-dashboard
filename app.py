e696ace1a5e05c7d788b93bda52c711568de30a3
alexmahesh <mail@alexanderschuppe.de>
2024-02-12 23:09:16 +0100

    Worked on a notebook to prepare the dashboard.

--- app.py
+++ app.py
@@ -2,15 +2,40 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
+
+@st.cache_data
+def get_data():
+    # Connect to Google Sheet
+    con = st.connection('gsheets', type=GSheetsConnection)
+    # Read Data and return Pandas Dataframe
+    return con.read(worksheet='Bewerbungen', usecols=list(range(8)), ttl=5)
+    
+
 st.title('Bewerbungs-Dashboard')
 st.markdown('Statistik meiner Bewerbungen als Data Analyst')
 
-# Verbindung zum Google Spreadsheet herstellen
-con = st.connection('gsheets', type=GSheetsConnection)
+# Read and Clean Data
+data = get_data()
+data.dropna(how='all', inplace=True) #Leerzeilen löschen
+data['Datum'] = pd.to_datetime(data['Datum'], format='%d.%m.%Y') #Datumstyp setzen
 
-# Daten lesen, worksheet=Name des Spreadsheet, usecols=Anzahl der zu verwendenden Spalten, 
-# ttl=5 sekunden (wie oft die Daten aktualisiert werden, Standard ist 60 Min.)
-# Gibt autom. Pandas Dataframe zurück
-data = con.read(worksheet='Bewerbungen', usecols=list(range(8)), ttl=5)
-data.dropna(how='all', inplace=True)
+# Calculate Statistics for Dashboard
+anz_abs = data['Datum'].count() #Absolute Bewerbungen
+anz_offen = data[data['Resultat']=='Offen']['Resultat'].count() #Offene Bewerbungen
+anz_absagen = data[data['Resultat']=='Absage']['Resultat'].count() #Absagen
 
+# Output
+st.dataframe(data)
+st.write(f'Anzahl Bewerbungen abs: {anz_abs}') 
+st.write(f'Anzahl Bewerbungen offen: {anz_offen}') 
+st.write(f'Anzahl Bewerbungen absagen: {anz_absagen}') 
+
+# Anzahl durchschnittlich pro ?
+# Anzahl pro Tag
+# Anzahl pro Woche
+# Anzahl pro Monat
+# Anzahl offen
+# Anzahl Gespräche
+# Anzahl Bewerbungen Plattformen
+# Anzahl Bewerbungen Initiativ
+# Anzahl Bewerbungen Dienstleister
